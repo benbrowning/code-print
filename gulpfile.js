c9e76702e09a5a0ad3d2b7b91d25233f60c585d4
Ben Browning <ben@bright-interactive.co.uk>
2014-11-05 16:54:38 +0000

    Getting a gulp file that works properly
    Sass watch task that actually works!
    Deleted unused plugins from packages.conf

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
-/* global karma */
-var gulp = require('gulp'),
-    karma = require('karma').server,
-    sass = require('gulp-sass'),
-    watch = require('gulp-watch')
+
+var gulp = require('gulp'),//http://gulpjs.com/
+    util = require('gulp-util'),//https://github.com/gulpjs/gulp-util
+    sass = require('gulp-sass'),//https://www.npmjs.org/package/gulp-sass
+    autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+    log = util.log;
+
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
 
 /**
  * Run test once and exit
@@ -38,9 +52,22 @@ var gulp = require('gulp'),
 //     // `one` and `two` will contain same files
 // });
 
-gulp.task('watch', function () {
-    watch('pages/sass/*.scss')
-        // .pipe(plumber())
-        .pipe(sass())
+var sassFiles = 'pages/sass/**/*.scss';
+
+gulp.task('sass', function(){
+    log("Generate CSS files " + (new Date()).toString());
+    gulp.src(sassFiles)
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 3 version','safari 5', 'ie 8', 'ie 9'))
         .pipe(gulp.dest('pages/css/'));
+        // .pipe(rename({suffix: '.min'}))
+        // .pipe(minifycss())
+        // .pipe(gulp.dest('target/css'));
 });
+
+
+gulp.task('watch', function(){
+    log('Watching scss files for modifications');
+    gulp.watch(sassFiles, ['sass']);
+});
+
