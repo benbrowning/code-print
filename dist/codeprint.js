46f415422410522f414acd85b4dd449904b526a6
Ben Browning <ben@bright-interactive.co.uk>
2015-12-24 14:26:42 +0000

    Rename codeprint to codePrint so filename matches module name.

--- dist/codeprint.js
+++ dist/codeprint.js
@@ -1,14 +1,35 @@
-/*! codeprint - v0.0.0
- *  Release on: 2014-10-22
- *  Copyright (c) 2014 Ben Browning
- *  Licensed MIT */
-var codePrint;
+/*
+ --------------------------------------------------------------------
+ Code Print
+ --------------------------------------------------------------------
+ */
 
-codePrint = (function ($) {
+// Use a UMD pattern that allows codePrint to work in AMD, CommonJS and normal
+// browser environments
+// (based on: https://github.com/umdjs/umd/blob/master/templates/returnExportsGlobal.js)
+
+(function (root, factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], function (jquery) {
+            return (root.codePrint = factory(jquery));
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory(require('jQuery'));
+    } else {
+        // Browser globals
+        root.codePrint = factory(root.jQuery);
+    }
+}(this, function ($) {
+
     'use strict';
 
     var defaults = {
-        usePrism: 'false',
         wrappingDivClass: 'sg-block'
     };
 
@@ -19,8 +40,8 @@ codePrint = (function ($) {
         var settings = $.extend(defaults, options);
         $('[data-codeprint]').each(handleSingle);
         $('[data-codeprint-group]').each(handleGrouped);
-        if (settings.usePrism) {
-            Prism.highlightAll();
+        if (settings.onComplete) {
+            settings.onComplete();
         }
     }
 
@@ -39,29 +60,41 @@ codePrint = (function ($) {
     }
 
     function createSourceBlock($component, componentHTML) {
-        var $sourcePanel,
-            $codeEl;
-
         // Create wrapping div
         $component.wrap('<div class="sg-block"></div>');
+        // insert the generated code block panel after the component
+        $component.after(getCodeBlockPanel(componentHTML));
+    }
 
-        $codeEl = $('<code class="language-markup"></code>');
+    function getCodeBlockPanel(componentHTML) {
+        var $codeEl = $('<code class="language-markup"></code>');
+
         $codeEl.text(componentHTML);
-        $sourcePanel = $('<pre class="code"></pre>').append($codeEl);
-        $component.after($sourcePanel);
+        return $('<pre class="code"></pre>').append($codeEl);
     }
 
     function getSourceCode($elem, isGrouped) {
-        // Don't want to include the data attribute in the sorce code
+        // Don't want to include the data attribute in the source code
         var $cleanComponent = $elem.removeAttr('data-codeprint'),
-            html = isGrouped ? $cleanComponent[0].innerHTML : $cleanComponent[0].outerHTML,
-            regex = /^\s/g;
+            html = isGrouped ? $cleanComponent[0].innerHTML : $cleanComponent[0].outerHTML;
+
+        html = trimWhitespace(html);
+
         // Return html having stripped out any leading whitespace
-        return html.replace(regex, '');
+        return html;
+    }
+
+    function trimWhitespace(string) {
+        var regexTrailingLeading = /^\s+|\s+$/g;
+
+        return string.replace(regexTrailingLeading, '');
     }
 
     return {
-        init:init
+        init: init,
+        getSourceCode: getSourceCode,
+        trimWhitespace: trimWhitespace,
+        getCodeBlockPanel: getCodeBlockPanel
     };
+}));
 
-}(jQuery));
